fe5f1d7c077b6942ddbbb8447c9ecf53087eabb2
MauroGomezDev <133311013+MauroGomezDev@users.noreply.github.com>
2023-11-28 10:48:17 -0300

    Se elimina codigo comentado

--- evaluacion/src/main/java/com/globallogic/evaluacion/controller/EvaluacionController.java
+++ evaluacion/src/main/java/com/globallogic/evaluacion/controller/EvaluacionController.java
@@ -1,9 +1,6 @@
 package com.globallogic.evaluacion.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.globallogic.evaluacion.exceptions.InvalidDataException;
-import com.globallogic.evaluacion.exceptions.ResponseException;
-import com.globallogic.evaluacion.exceptions.UserAlreadyExistsException;
 import com.globallogic.evaluacion.jwt.UserResponse;
 import com.globallogic.evaluacion.model.User;
 import com.globallogic.evaluacion.service.UserService;
@@ -11,7 +8,6 @@ import com.globallogic.evaluacion.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +18,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/evaluacion")
-//@CrossOrigin("*")
-//@CrossOrigin(origins = "http://localhost:8080")
-//@ComponentScan(basePackages = "com.globallogic")
 public class EvaluacionController {
 
     private static final Logger log = LoggerFactory.getLogger(EvaluacionController.class);
